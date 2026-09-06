75b8208de810c55297e5d847908a778982a46558
Faytlla <faytlla@example.com>
2026-09-06 17:28:29 +0700

    fix: add !important to nav-header color rule

--- public/themes/adminlte/index.php
+++ public/themes/adminlte/index.php
@@ -29,7 +29,7 @@
     ?>
 
     <link rel="stylesheet" href="<?php echo base_url('assets/css/fashioner-admin.css?v=8'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/fashioner-dashboard.css?v=20'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/fashioner-dashboard.css?v=21'); ?>">
 
     <script type="text/javascript" async>
     var run_title_text = " <?=$title_text?> ";
