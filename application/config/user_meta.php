ad68d5f39a54b5d5ebc5a7b610eca3678a3cd42c
Faytlla <faytlla@example.com>
2026-09-10 09:53:30 +0700

    fix: ubah validasi country pada user_meta menjadi opsional

--- application/config/user_meta.php
+++ application/config/user_meta.php
@@ -39,7 +39,7 @@ $config['user_meta_fields'] =  array(
 	array(
 		'name'   => 'country',
 		'label'   => lang('user_meta_country'),
-		'rules'   => 'required|trim|max_length[100]',
+		'rules'   => 'trim|max_length[100]',
         'admin_only'    => false,
 		'form_detail' => array(
 			'type' => 'country_select',
